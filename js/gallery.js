fa66083829d3f1a6505ee94e01769c9c66930f27
Kai Yuto <k.yuto199688@gmail.com>
2026-06-19 20:24:39 +0900

    Finish the JavaScript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,26 @@
 function activateGallery() {
-  alert("ギャラリーファイルからこんにちは！");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo img");
+  const currentClass = "current";
+  // 更新される画像情報
+  let galleryInfo = document.querySelector("#gallery-info");
+  let title = galleryInfo.querySelector(".title");
+  let description = galleryInfo.querySelector(".description");
+
+  thumbnails.forEach(function(thumbnail){
+    thumbnail.addEventListener("click", function(){
+      // クリックされたサムネイル画像をメイン画像として設定する
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", thumbnail.alt);
+
+      // 現在の画像を変更する
+      document.querySelector(".current").classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+
+      // 画像の情報を更新する
+      title.innerHTML = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
+  });
 }
